680ffce3e576f27e14ab8605ab2d9f044d007b8d
Greg Garcia <greg.garcia@greg.garcia>
2019-05-13 11:12:05 -0500

    added fixtures

---
+++ fixtures.js
@@ -0,0 +1,44 @@
+var users = 
+	{	id: 'greg.garcia', 
+		email: 'greg.garcia@bazaarvoice.com', 
+		subscriptions: ['csttires', 'hasbro'] 
+	},
+	{	id: 'stephanie.gage', 
+		email: 'stephanie.gage@bazaarvoice.com', 
+		subscriptions: ['walmart', 'target']
+	},
+	{	id: 'brian.chang', 
+		email: 'greg.garcia@bazaarvoice.com', 
+		subscriptions: ['csttires', 'walmart']
+	};
+
+var clients = {
+	{
+		id: 'csttires',
+		pie_status: ['fail', {}],
+		feed_status: ['stale', {}],
+		display_status: ['pass', {}],
+		pixel_status: ['pass', {}],
+	},
+	{
+		id: 'hasbro',
+		pie_status: ['pass', {}],
+		feed_status: ['pass', {}],
+		display_status: ['pass', {}],
+		pixel_status: ['pass', {}],
+	},
+	{
+		id: 'walmart',
+		pie_status: ['fail', {}],
+		feed_status: ['fail', {}],
+		display_status: ['fail', {}],
+		pixel_status: ['fail', {}],
+	}
+		{
+		id: 'target',
+		pie_status: ['pass', {}],
+		feed_status: ['pass', {}],
+		display_status: ['fail', {}],
+		pixel_status: ['fail', {}],
+	}
+}
